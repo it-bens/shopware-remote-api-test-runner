34ef43cfd06534e17825dd7ef45a4bde4ae114e5
Martin Bens <martin.bens@it-bens.de>
2025-02-20 19:25:30 +0100

    Conditional argument for `ShopConfigurator` added in `AfterInstallConfigurator` to comply with Shopware 6.6.10.0 and above.

--- code/src/Setup/AfterInstallConfigurator.php
+++ code/src/Setup/AfterInstallConfigurator.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace ITB\ShopwareRemoteApiTestRunner\Setup;
 
+use Composer\InstalledVersions;
+use Composer\Semver\Comparator;
 use Doctrine\DBAL\Connection;
+use Psr\EventDispatcher\EventDispatcherInterface;
 use Shopware\Core\Framework\Test\TestCaseBase\AdminApiTestBehaviour;
 use Shopware\Core\Framework\Test\TestCaseBase\IntegrationTestBehaviour;
 use Shopware\Core\Maintenance\System\Service\ShopConfigurator;
@@ -14,23 +17,36 @@ final class AfterInstallConfigurator
     use AdminApiTestBehaviour;
     use IntegrationTestBehaviour;
 
-    public function changeSystemCurrency(string $currency): void
+    private readonly ShopConfigurator $shopConfigurator;
+
+    public function __construct()
     {
+        $shopConfiguratorConstructorArguments = [];
+
         /** @var Connection $connection */
         $connection = $this->getBrowser()
             ->getContainer()
             ->get(Connection::class);
-        $shopConfigurator = new ShopConfigurator($connection);
-        $shopConfigurator->setDefaultCurrency($currency);
+        $shopConfiguratorConstructorArguments[] = $connection;
+
+        if (Comparator::greaterThanOrEqualTo(InstalledVersions::getVersion('shopware/platform'), '6.6.10.0')) {
+            /** @var EventDispatcherInterface $eventDispatcher */
+            $eventDispatcher = $this->getBrowser()
+                ->getContainer()
+                ->get(EventDispatcherInterface::class);
+            $shopConfiguratorConstructorArguments[] = $eventDispatcher;
+        }
+
+        $this->shopConfigurator = new ShopConfigurator(...$shopConfiguratorConstructorArguments);
+    }
+
+    public function changeSystemCurrency(string $currency): void
+    {
+        $this->shopConfigurator->setDefaultCurrency($currency);
     }
 
     public function changeSystemLanguage(string $locale): void
     {
-        /** @var Connection $connection */
-        $connection = $this->getBrowser()
-            ->getContainer()
-            ->get(Connection::class);
-        $shopConfigurator = new ShopConfigurator($connection);
-        $shopConfigurator->setDefaultLanguage($locale);
+        $this->shopConfigurator->setDefaultLanguage($locale);
     }
 }
